a1e5233dfb09cb06d30dc217e588cf0fd13204ac
Ramshley P. <ramshleyv@gmail.com>
2025-04-05 12:07:43 -0400

    Agregación: se agregó la clase usuario

---
+++ Domain/Models/Usuario.cs
@@ -0,0 +1,19 @@
+﻿namespace Domain.Models
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Email { get; set; }
+        public string PasswordHash { get; set; }
+        public int RolId { get; set; } //Llave foranea de la tabla rol
+        public bool Activo { get; set; }
+        public DateTime CreadoEn { get; set; }
+        public DateTime ActualizadoEn { get; set; }
+        
+        //Navegacion usuario puede tener un rol
+        public virtual Rol Rol { get; set; }
+
+    }
+}
