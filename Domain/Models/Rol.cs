6c4b3e00602294d5b5c4fc0f81af27b7962ca75a
Ramshley P. <ramshleyv@gmail.com>
2025-04-05 12:03:46 -0400

    agregarción: clase rol en el domain

---
+++ Domain/Models/Rol.cs
@@ -0,0 +1,14 @@
+﻿namespace Domain.Models
+{
+    public class Rol
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public DateTime CreadoEn { get; set; }
+        public DateTime ActualizadoEn { get; set; }
+
+        //Navegacion rol puede tener muchos usuarios e inicializado vacia
+        public virtual ICollection<Usuario> Usuarios { get; set; } = new HashSet<Usuario>();
+    }
+}
