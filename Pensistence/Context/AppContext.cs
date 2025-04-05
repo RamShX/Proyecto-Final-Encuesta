5157449c3ec4eb34b03a196336efb7a344c8a877
Ramshley P. <ramshleyv@gmail.com>
2025-04-05 17:47:13 -0400

    cambio y agregación del context

---
+++ Pensistence/Context/AppContext.cs
@@ -0,0 +1,28 @@
+﻿using Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Pensistence.Context
+{
+    public class AppContext : DbContext
+    {
+        public AppContext(DbContextOptions<AppContext> options) : base(options)
+        {
+        }
+        public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<Rol> Roles { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppContext).Assembly);
+
+            //Configurar la relación entre Usuario y Rol
+            modelBuilder.Entity<Usuario>()
+                .HasOne(u => u.Rol)
+                .WithMany(r => r.Usuarios)
+                .HasForeignKey(u => u.RolId);
+        }
+    }
+    
+}
+
