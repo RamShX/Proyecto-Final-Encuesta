f0852e3ac13708afd1e2e71f3e5c6f68f97499af
Ramshley P. <ramshleyv@gmail.com>
2025-04-18 01:46:41 -0400

    Agregación: implementar interfaz pregunta

---
+++ Domain/Interfeces/IPreguntaService.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Models;
+
+namespace Domain.Interfeces
+{
+    public interface IPreguntaService
+    {
+        Task<bool> CrearPregunta(Pregunta pregunta);
+        Task<bool> ActualizarPregunta(Pregunta pregunta);
+        Task<bool> EliminarPregunta(int id);
+        Task<List<Pregunta>> GetPreguntasByEncuestaId(int encuestaId);
+    }
+}
